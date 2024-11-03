52ea7be9b6851e5695c9c7b23ac27a0ded0cf85b
Raphyo <raphaelvannerom@gmail.com>
2024-11-03 20:08:32 +0200

    finished trainig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+ 
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+    turbo: {
+      rules: {
+        // Use glob patterns instead of extensions
+        // Example: "*.mdx": ["mdx-loader"]
+      }
+    }
+  },
 };
-
-export default nextConfig;
+ 
+module.exports = nextConfig;
