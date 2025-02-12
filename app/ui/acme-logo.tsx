8f8b0397f42c4a0587acf9f74a3b36cbdf2bdcf5
raphaelvannerom <raphaelvannerom@gmail.com>
2025-02-12 16:31:43 +0200

    logo issue

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image';
 
 export default function ZecallLogo() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center justify-center">
       <Image
-        src="/logo.jpeg"
+        src="/logo.png"
         alt="Zecall.ai Logo"
         width={250}
         height={84}
         priority
+        unoptimized
       />
     </div>
   );
