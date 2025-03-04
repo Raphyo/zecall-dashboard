e5917e802b7d633809860f936e7c91809aecc208
raphaelvannerom <raphaelvannerom@gmail.com>
2025-03-04 11:42:46 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Providers } from './providers';
+ import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'zecall.ai',
