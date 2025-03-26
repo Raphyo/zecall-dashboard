1910661d78a8bc26d682c76fafcbcce2444e3294
raphaelvannerom <raphaelvannerom@gmail.com>
2025-03-26 23:03:09 +0200

    try

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,9 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
-const auth = NextAuth(authConfig).auth;
-
-export default async function middleware(req: any) {
-  // Bypass auth for Stripe webhook
-  if (req.nextUrl.pathname === '/api/stripe/webhook') {
-    return;
-  }
-
-  // Apply auth middleware for all other routes
-  return auth(req);
-}
+export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/((?!api/stripe/webhook|_next/static|_next/image|.*\\.png$).*)'],
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
